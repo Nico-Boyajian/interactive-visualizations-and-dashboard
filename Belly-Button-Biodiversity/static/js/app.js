ba7c08360be6b1852dd379d8507a04646ee1aa9d
Nico Boyajian <njboyajian@gwu.edu>
2020-03-12 12:24:12 -0400

    changes

--- Belly-Button-Biodiversity/static/js/app.js
+++ Belly-Button-Biodiversity/static/js/app.js
@@ -11,11 +11,12 @@ function buildMetadata(sample) {
   // Hint: Inside the loop, you will need to use d3 to append new
   // tags for each key-value in the metadata.
   d3.json(metadataURL).then(function (data) {
+    
     Object.entries(data).forEach(([key, value]) => {
       panelMetadata.append("h6").text(`${key}: ${value}`
       );
     })
-    
+
 
 
     // BONUS: Build the Gauge Chart
